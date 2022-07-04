432d1ae41aacc90d38c328a92ed180e8f834c00f
Nikolas Rist <nikolas.rist@leanix.net>
2022-07-04 09:26:50 +0200

    Improve DAO implementation

--- src/domain/foo.dao.ts
+++ src/domain/foo.dao.ts
@@ -5,11 +5,11 @@ export type DAO = typeof fooDAO;
 
 export const fooDAO = {
   insert: async (pg: Knex, item: FooUpdate): Promise<Foo> => {
-    const items = await pg<Foo>('foo').insert(item).returning('*');
+    const items = await pg.table<Foo>('foo').insert(item).returning('*');
     return items[0];
   },
 
   delete: async (pg: Knex, itemId: string): Promise<number> => {
-    return await pg<number>('foo').where('id', itemId).del();
+    return await pg.table<number>('foo').where('id', itemId).del();
   }
 };
