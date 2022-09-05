106fc574abe5e184453a9f33bb75302bc47c447e
Nikolas Rist <nikolas.rist@leanix.net>
2022-09-05 16:55:47 +0200

    Update typing

--- src/domain/foo.mapper.ts
+++ src/domain/foo.mapper.ts
@@ -1,6 +1,6 @@
 import { Foo2 } from './foo.models';
 
-export function mapNameToFoo2(name: string, age?: number | undefined): Foo2 {
+export function mapNameToFoo2(name: string, age: number | undefined): Foo2 {
   return age
     ? {
         id: 'foo2Id',
